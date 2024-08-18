d169ccb5a626397e67470124754982fb31020014
ShawnDeng-code <88145482@qq.com>
2024-08-18 15:33:00 +0800

    feat: add github action

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
+const isProd = process.env.NODE_ENV === 'production'
 
 const cfg = {
   ...withNextra(),
-  basePath: '/shawndeng_blogs',
+  basePath: isProd ? '/shawndeng_blogs' : "",
   output: "export",
   images: {
     unoptimized: true,
