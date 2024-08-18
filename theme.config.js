eca42ddcb5390d2f541ceff9265ddcc085709099
ShawnDeng-code <88145482@qq.com>
2024-08-18 15:49:45 +0800

    feat: add github action

--- theme.config.js
+++ theme.config.js
@@ -2,19 +2,21 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Shawn Deng.
-      <a href="/feed.xml">RSS</a>
+    <small style={{display: 'block', marginTop: '8rem'}}>
+      <time>{YEAR}</time>
+      © Shawn Deng.
+      <a href={`${process.env.NODE_ENV === 'production' ? '/shawndeng_blogs' : ''}/feed.xml`}>RSS</a>
       <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
+          a {
+              float: right;
           }
-        }
+
+          @media screen and (max-width: 480px) {
+              article {
+                  padding-top: 2rem;
+                  padding-bottom: 4rem;
+              }
+          }
       `}</style>
     </small>
   )
