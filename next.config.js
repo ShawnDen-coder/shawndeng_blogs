13c9c48816976232caa73a933fa950e0a63df527
ShawnDeng-code <88145482@qq.com>
2024-08-18 14:20:05 +0800

    feat: add github action

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
 const cfg = {
   ...withNextra(),
+  output: "export",
   images: {
     unoptimized: true,
   }
+
 }
 
 module.exports = cfg
